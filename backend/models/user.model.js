8e565474109ddedc86d653a1edeafa0de2723f1b
imtithalhalalo <halaloimtithal@gmail.com>
2022-12-10 23:29:10 +0200

    backend: adding validation for email

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -13,6 +13,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: 'email is required',
         unique: true,
+        validate: {
+            validator: (value) => {
+              const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+              return value.match(re);
+            },
+            message: "Please enter a valid email address",
+        },
         trim: true,
     },
     password: {
