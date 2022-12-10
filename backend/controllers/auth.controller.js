872b54294782b62efa19bdca87111e97085eb5cc
imtithalhalalo <halaloimtithal@gmail.com>
2022-12-11 00:39:53 +0200

    backend: adding signin and info apis

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -24,8 +24,38 @@ const signup = async (req, res) => {
     }
 };
 
+const signin = async (req, res)=>{
+  const {email, password} = req.body;
+  
+  const user = await User.findOne({email}).select("+password");
+  if(!user) return res.status(404).json({message: "Invalid Credentials"});
+  if(user.banned === true) return res.status(400).json({message: "you are banned from the admin"});
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch) return res.status(404).json({message: "Invalid Credentials"});
+
+  const token = jwt.sign({email: user.email, name: user.name}, process.env.JWT_SECRET_KEY, {
+      expiresIn: '1h'
+  });
+  res.status(200).json({
+      'status': "success",
+      'user':user,
+      'token':token
+  })
+}
+
+const userInfo = async (req, res) => {
+  try {
+    const currentUser = req.user;
+    res.status(200).json(currentUser);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
 
 // export all the functions
 module.exports = {
+    signin,
     signup,
+    userInfo,
 };
